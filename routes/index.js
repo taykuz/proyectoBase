26bd4f497a8c8cbf5dd668f555febc3053781d41
taykuz <zaycrix05@gmail.com>
2016-12-09 13:21:57 -0400

    creacion objeto reclamo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var reclamo = {tipoReclamo: 'odeco',
+              observacion: 'Mal servicio',
+              fecha: '26/11/2016'}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Avances de Computacion' });
+  res.render('index', { title: 'Proyecto Final', subtitle: 'Avances de computacion', reclamo: reclamo});
 });
 
 module.exports = router;
