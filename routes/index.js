2508cd6e0a80d0d7f3deada18e395ad2e9a9b4a4
taykuz <zaycrix05@gmail.com>
2016-12-09 12:38:56 -0400

    primeros cambios proyecto final

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Avances de Computacion' });
 });
 
 module.exports = router;
